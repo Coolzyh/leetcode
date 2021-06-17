085d52ec76f6eadb8bd1ff6afce08b5a7bf2b8bb
Coolzyh <heziyan100@gmail.com>
2021-06-16 23:12:31 -0400

    Problem 144 Tree Preorder Traversal

---
+++ [144] Binary Tree Preorder Traversal/144.binary-tree-preorder-traversal.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=144 lang=cpp
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> st;
+        while (root != nullptr || !st.empty()) {
+            while (root != nullptr) {
+                res.push_back(root->val);
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            root = root->right;
+        }
+        return res;
+    }
+};
+// @lc code=end
+
---
+++ [144] Binary Tree Preorder Traversal/144.binary-tree-preorder-traversal.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode id=144 lang=java
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        while (root != null || !st.empty()) {
+            while (root != null) {
+                res.add(root.val);
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+            root = root.right;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
