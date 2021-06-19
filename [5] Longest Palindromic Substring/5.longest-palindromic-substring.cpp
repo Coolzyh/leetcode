0edd5d93b86b9e7b1efca1900c740764f8c1d39c
Coolzyh <heziyan100@gmail.com>
2021-06-19 16:34:02 -0400

    Problem 5 cpp

--- [5] Longest Palindromic Substring/5.longest-palindromic-substring.cpp
+++ [5] Longest Palindromic Substring/5.longest-palindromic-substring.cpp
@@ -8,7 +8,29 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        
+        if (s.length() == 0) return s;
+        int start = 0;
+        int res_len = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandfromCenter(i, i, s);
+            int len2 = expandfromCenter(i, i+1, s);
+            int len = max(len1, len2);
+            if (res_len < len) {
+                res_len = len;
+                start = (i + len/2)-len+1;  //start = end-len+1
+            }
+        }
+        return s.substr(start, res_len);
+    }
+
+    int expandfromCenter(int left, int right, string s) {
+        while (left >= 0 && right < s.length()) {
+            if (s[left] == s[right]) {
+                left--;
+                right++;
+            } else break;
+        }
+        return right-left-1;
     }
 };
 // @lc code=end
