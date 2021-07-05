cdfd3a6345166f42eeda752b44abd69dc301bb65
Coolzyh <heziyan100@gmail.com>
2021-07-04 21:41:29 -0400

    Problem 90

--- [90] Subsets II/90.subsets-ii.java
+++ [90] Subsets II/90.subsets-ii.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
  * @lc app=leetcode id=90 lang=java
  *
@@ -7,7 +9,19 @@
 // @lc code=start
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        int nums_len = nums.length;
+        for (int i = 0; i < nums_len; i++) {
+            int res_len = res.size();
+            for (int j = 0; j < res_len; j++) {
+                List<Integer> current = new ArrayList<>(res.get(j));
+                current.add(nums[i]);
+                res.add(new ArrayList<>(current));
+            }
+        }
+        return res;
     }
 }
 // @lc code=end
