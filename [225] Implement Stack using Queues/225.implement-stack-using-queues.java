6e036e77e84d777da9acbe1f6605e1e81c01a2b8
Coolzyh <heziyan100@gmail.com>
2021-07-14 16:34:59 -0400

    Problem 225

---
+++ [225] Implement Stack using Queues/225.implement-stack-using-queues.java
@@ -0,0 +1,58 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * @lc app=leetcode id=225 lang=java
+ *
+ * [225] Implement Stack using Queues
+ */
+
+// @lc code=start
+class MyStack {
+
+    /** Initialize your data structure here. */
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        q2.add(x);
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+        Queue<Integer> tmp = q1;
+        q1 = q2;
+        q2 = tmp;
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return q1.remove();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return q1.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+// @lc code=end
+
